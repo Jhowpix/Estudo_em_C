adf34045b42e13ffec71f7b7f665d4a575034292
Jhonatan de Oliveira <jhonatandevbr@gmail.com>
2023-04-08 20:30:05 +0900

    .
    .

---
+++ index.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+  printf("Hello, World!\n");
+  // para compilar F5  
+  // Ctrl + Alt + N
+  char nome;
+  printf("Qual seu nome?\n");
+  scanf(nome);
+  printf("Seja bem vindo ", nome,"\n");
+
+}
